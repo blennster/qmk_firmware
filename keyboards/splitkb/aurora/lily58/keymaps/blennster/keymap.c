b67f0a9369ac4de28415f4fbb8eed24088a81082
Emil Blennow <emil.blennow99@gmail.com>
2024-02-09 08:41:14 +0100

    update keymap

--- keyboards/splitkb/aurora/lily58/keymaps/blennster/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/blennster/keymap.c
@@ -22,10 +22,11 @@ _______ ,KC_NUBS ,_______ ,_______   ,_______ ,_______   ,LSFT(KC_NUBS) ,RALT(KC
 /*s     ,s       ,s       ,*/_______ ,_______ ,_______   ,_______       ,_______       ,MO(3)      ,KC_DEL     ,_______ /* ,s          ,s       ,s       ,*/
     ),
     [2] = LAYOUT(
+        // KC_EQL for ticks and backticks on swedish layout
 _______ ,_______ ,_______ ,_______   ,_______ ,_______  /*,mcu     ,mcu*/   ,_______ ,_______ ,_______   ,_______  ,_______ ,KC_VOLU ,
 KC_GRV  ,KC_1    ,KC_2    ,KC_3      ,KC_4    ,KC_5     /*,mcu     ,mcu*/   ,KC_6    ,KC_WH_U ,KC_8      ,KC_9     ,KC_MPRV ,KC_VOLD ,
-KC_F1   ,KC_F2   ,KC_F3   ,KC_F4     ,KC_F5   ,KC_F6    /*,mcu     ,mcu*/   ,KC_LEFT ,KC_DOWN ,KC_UP     ,KC_RIGHT ,XXXXXXX ,XXXXXXX ,
-KC_F7   ,KC_NUBS ,KC_F9   ,KC_F10    ,KC_F11  ,KC_F12     ,_______ ,_______ ,KC_MNXT ,KC_WH_D ,KC_EQL    ,KC_LBRC  ,KC_MPLY ,_______ ,
+_______ ,KC_F2   ,KC_F3   ,KC_F4     ,KC_F5   ,KC_F6    /*,mcu     ,mcu*/   ,KC_LEFT ,KC_DOWN ,KC_UP     ,KC_RIGHT ,XXXXXXX ,XXXXXXX ,
+_______ ,KC_NUBS ,KC_F9   ,KC_F10    ,KC_F11  ,KC_F12     ,_______ ,KC_EQL  ,KC_MNXT ,KC_WH_D ,_______   ,_______  ,KC_MPLY ,_______ ,
 /*s     ,s       ,s       ,*/_______ ,_______ ,MO(3)      ,_______ ,_______ ,_______ ,KC_DEL  ,_______/* ,s        ,s       ,s       ,*/
     ),
     [3] = LAYOUT(
